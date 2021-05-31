996f3fbbec6fc7a3b747e761f90bf4ef10aeca2a
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-05-31 09:09:58 +0900

    Add files via upload

---
+++ subject_CodeOnly11/subject_CodeOnly11/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  subject_CodeOnly11
+//
+//  Created by 長谷川孝太 on 2021/05/30.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    static let identifier = "CustomTableViewCell"
+
+}
